40e9bca79e005e7f1cfd641f1d12c4e19baaaba6
Mateus Carvalho <mtsbarbosa@hotmail.com>
2022-04-24 13:54:54 -0300

    * Fixing url on project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.majorcluster/datomic-helper "0.1.0"
+(defproject org.clojars.majorcluster/datomic-helper "0.1.1"
   :description "A Clojure Library with tools to help using datomic"
   :url "https://clojars.org/org.clojars.majorcluster/datomic-helper"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
