50d55022c3599d4960f950d4e8ef6e4cb17ab9b7
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-01-07 12:51:58 -0300

    Bumped deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.majorcluster/datomic-helper "1.1.0"
+(defproject org.clojars.majorcluster/datomic-helper "1.2.0"
   :description "A Clojure Library with tools to help using datomic"
   :url "https://github.com/mtsbarbosa/datomic-helper"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [prismatic/schema "1.1.12"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [prismatic/schema "1.4.1"]
                  [com.datomic/datomic-free "0.9.5697"]]
   :profiles {:dev {}})
