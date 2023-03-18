09f116bbf5f3fc8100740555acd262ecc0c2e98d
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-03-18 15:29:17 -0300

    Adjusting license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.clojars.majorcluster/datomic-helper "2.0.0"
+(defproject org.clojars.majorcluster/datomic-helper "2.0.1"
   :description "A Clojure Library with tools to help using datomic"
   :url "https://github.com/mtsbarbosa/datomic-helper"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [prismatic/schema "1.4.1"]
                  [com.datomic/datomic-free "0.9.5697"]]
