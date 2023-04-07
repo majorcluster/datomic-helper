1668fd3fc54d63a02fcf8eb4a2ea34ad1424c6b2
Mateus Carvalho <1434224+mtsbarbosa@users.noreply.github.com>
2023-04-06 23:23:54 -0300

    Adding deployment script

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.majorcluster/datomic-helper "3.0.0"
+(defproject org.clojars.majorcluster/datomic-helper "3.0.1"
   :description "A Clojure Library with tools to help using datomic"
   :url "https://github.com/majorcluster/datomic-helper"
   :license {:name "The MIT License"
@@ -6,6 +6,9 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [prismatic/schema "1.4.1"]
                  [com.datomic/datomic-free "0.9.5697"]]
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password}]]
   :profiles {:dev {:plugins [[com.github.clojure-lsp/lein-clojure-lsp "1.3.17"]]}}
   :aliases {"diagnostics"     ["clojure-lsp" "diagnostics"]
             "format"          ["clojure-lsp" "format" "--dry"]
